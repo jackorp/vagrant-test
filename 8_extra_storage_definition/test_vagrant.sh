3575da3e0c894f0642df6967cb5f60e8c7db90b2
Jarek Prokop <jprokop@redhat.com>
2023-02-03 14:01:23 +0100

    Quoting and trap fix

--- 8_extra_storage_definition/test_vagrant.sh
+++ 8_extra_storage_definition/test_vagrant.sh
@@ -30,7 +30,10 @@ function cleanup() {
   virsh vol-list --pool default | grep "${vol_match}" | cut -d' ' -f2 | xargs -rn1 virsh vol-delete --pool default
 }
 
-trap "vagrant destroy -f; as_unprivileged_user cleanup" ERR
+trap 'vagrant destroy -f;
+as_unprivileged_user vol_match=$(basename $(pwd));
+virsh vol-list --pool default | grep "${vol_match}" | cut -d" " -f2 | xargs -rn1 virsh vol-delete --pool default
+' ERR
 
 function as_unprivileged_user() {
   current_pwd=$(pwd)
